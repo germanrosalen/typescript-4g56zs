44437d681afb000f3748936e96ca348677f89adb
germanrosalen <grosalen@gmail.com>
2022-08-08 18:14:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-4g56zs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-4g56zs)
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+class persona{
+  //atributos de la clase persona
+private nombre: string;
+private apellido: string;
+private añoNac: number;
+}
